94c177c46e638a7bb48b1e039099b2718fed2f73
Proweex <firlimoo1@gmail.com>
2022-05-25 11:43:53 +0700

    Changing random images

--- tracking.js
+++ tracking.js
@@ -5,40 +5,52 @@ const startEl = document.getElementById("start-btn");
 const stopEl = document.getElementById("stop-btn");
 const contentEL = document.getElementById("content");
 const warnEl = document.getElementById("warn");
-const geo = navigator.geolocation;
+const statEL = document.getElementById("status");
+
+const geoID = navigator.geolocation;
+
 
 let posLat = Array();
 let posLong = Array();
 let posList = Array();
 
+let stopTracking = true;
+
 if ('geolocation' in navigator) {
 // if (testing){
-    // warnEl.textContent = "enable";
+    stopEl.disabled = true;
 } else {
     contentEL.classList.add("removed");
     startEl.disabled = true;
     stopEl.disabled = true;
-    warnEl.textContent = "feature is not supported";
+    warnEl.textContent = "Browser does not support the Geolocation API";
 }
 
 function startTrack(){
-    console.log("Starting...")
-    geo.watchPosition((position) => {
+    console.log("Starting...");
+    startEl.disabled = true;
+    stopEl.disabled = false;
+
+    stopTracking = false;
+    geoID.watchPosition((position) => { 
         doSomething(position.coords.latitude, position.coords.longitude);
-      });
+    });
 }
 
 function stopTrack(){
     console.log("Stopped..")
-    navigator.geolocation.clearWatch(geo);
-    
-    posEl.innerHTML = posList;
+    startEl.disabled = false;
+    stopEl.disabled = true;
+
+    stopTracking = true;
+    navigator.geolocation.clearWatch(geoID);
+    statEL.textContent = "";
 }
 
 function doSomething(latitude, longitude){
+    console.log("Tracking")
     posLat.push(latitude);
     posLong.push(longitude);
-
-    posList.push("lat : " + latitude + " long: " + longitude + "<br>");
-    console.log(posList);
+    statEL.textContent = "Tracking";
+    posEl.innerHTML += latitude + " " + longitude + "<br>";
 }
